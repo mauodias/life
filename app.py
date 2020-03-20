84c27bcb0018e205bbbd8687905aea2178cac05f
Maurício Dias <mau.dias@gmail.com>
2020-03-20 23:44:18 +0100

    Creates cell class and adds one in the screen

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pygame
+from cell import Cell
 
 pygame.init()
 
@@ -6,12 +7,16 @@ screen = pygame.display.set_mode([500, 500])
 
 running = True
 
+cell = Cell(250, 250)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill((0,0,0))
+
+    pygame.draw.circle(screen, (0,255,0), (cell.x, cell.y), 10)
 
     pygame.display.flip()
 
