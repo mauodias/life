a9ebfd93c8c96a22a59d8294228d301ce7ea2956
Maurício Dias <mau.dias@gmail.com>
2020-03-21 02:17:16 +0100

    Moves drawing to cell, drawing and walking action to cell tick.

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ screen = pygame.display.set_mode([500, 500])
 
 running = True
 
-cell = Cell(250, 250)
+cell = Cell(screen, x=250, y=250)
+cell2 = Cell(screen, x=250, y=250, speed=-1)
 
 while running:
     for event in pygame.event.get():
@@ -16,10 +17,10 @@ while running:
 
     screen.fill((0,0,0))
 
-    pygame.draw.circle(screen, cell.color, cell.position, cell.size)
+    cell.tick()
+    cell2.tick()
 
     pygame.display.flip()
-
-    cell.walk(30)
+    pygame.time.Clock().tick(60)
 
 pygame.quit()
--- cell.py
+++ cell.py
@@ -1,14 +1,16 @@
 from color import Color
 import math
+import pygame
 
 class Cell:
 
-    def __init__(self, x=0, y=0, size=10, color=Color.BLUE, speed=1):
+    def __init__(self, screen, x=0, y=0, size=10, color=Color.BLUE, speed=1):
         self._x = x
         self._y = y
         self._size = size
         self._color = color
         self._speed = speed
+        self._screen = screen
 
     @property
     def x(self):
@@ -45,3 +47,7 @@ class Cell:
     def walk(self, angle=0):
         self.x += math.cos(math.radians(angle))*self.speed
         self.y += math.sin(math.radians(angle))*self.speed
+
+    def tick(self):
+        pygame.draw.circle(self._screen, self.color, self.position, self.size)
+        self.walk()
